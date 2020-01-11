6b6e11b0ea0518b9d49f975c4ac87f0f0a2c7023
Lissett Munoz <lissettmck0888@gmail.com>
2020-01-10 22:36:21 -0400

    avance

--- src/main/java/com/gi/builmanager/config/SpringConfig.java
+++ src/main/java/com/gi/builmanager/config/SpringConfig.java
@@ -1,6 +1,7 @@
 package com.gi.builmanager.config;
 
 import com.gi.builmanager.converter.GastoComunDtoConverter;
+import com.gi.builmanager.converter.RolConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,5 +12,6 @@ public class SpringConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new GastoComunDtoConverter());
+        registry.addConverter(new RolConverter());
     }
 }
