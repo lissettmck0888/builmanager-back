bf032ef94dc1cfa2afbc6cfb95327ca0464b33bd
Victor Morales <vmorales@kdu.cl>
2020-01-20 18:24:32 -0300

    correccion query

--- src/main/java/com/gi/builmanager/repositorio/UnidadRepository.java
+++ src/main/java/com/gi/builmanager/repositorio/UnidadRepository.java
@@ -15,10 +15,29 @@ public interface UnidadRepository extends JpaRepository<Unidad,Integer> {
             "unidad u where idunidad not in (select idunidad from relacion_asignacion_unidad)", nativeQuery = true)
     List<Unidad> getUnidadesDisponiblesSinPropietario();
 
-    @Query(value = "select * from " +
-            "asignacion a " +
-            "join relacion_asignacion_unidad rau on a.id_asignacion=rau.idasignacion " +
-            "where a.tipo_asignacion= 'propietario' and estado='vacio'", nativeQuery = true)
+    @Query(value = "select\n" +
+            "*\n" +
+            "from\n" +
+            "unidad\n" +
+            "where\n" +
+            "idunidad in(\n" +
+            "select\n" +
+            "idunidad\n" +
+            "from\n" +
+            "(\n" +
+            "SELECT\n" +
+            "rau.idunidad,\n" +
+            "string_agg(a.tipo_asignacion,\n" +
+            "',') asignaciones\n" +
+            "FROM\n" +
+            "asignacion a\n" +
+            "JOIN relacion_asignacion_unidad rau ON\n" +
+            "a.id_asignacion = rau.idasignacion\n" +
+            "group by\n" +
+            "idunidad\n" +
+            "having\n" +
+            "asignaciones not ilike '%arriendo%' )\n" +
+            ");\n", nativeQuery = true)
     List<Unidad> getUnidadesDisponiblesParaArriendo();
 
 }
